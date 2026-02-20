eb0dd1724f328260bd90e417a9fbf9f10e12f1da
Peter Bakker <pe.bakker@flanderijn.nl>
2026-02-20 23:29:09 +0100

    imrpove Timeslot logging

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -95,7 +95,7 @@ public class ExampleTest : Setup
                     {
                         Console.WriteLine($"{location.locationId}_{location.LocationName}_{nlTime.ToString("yyyy-MM-dd_hh:mm:ss")}_{lane};{timeInString}; {location.locationId}; {location.LocationName}; {lane};  verhuurd; onbekend");
 
-                        CsvLogger.Log($"{location.locationId}_{location.LocationName}_{nlTime.ToString("yyyy-MM-dd_hh:mm:ss")}_{lane};{nlTime.ToString("yyyy-MM-dd")};{timeInString}; {nlTime.DayOfWeek.ToString()}; {location.locationId}; {location.LocationName}; {lane};  verhuurd; onbekend");
+                        CsvLogger.Log($"{location.locationId}_{location.LocationName}_{nlTime.ToString("yyyy-MM-dd_hh:mm:ss")}_{lane};{nlTime.ToString("yyyy-MM-dd")};{timeInString}; {nlTime.DayOfWeek.ToString()}; {location.locationId}; {location.LocationName}; {lane};  verhuurd; {await _pedelPom._firstPriceOfTimeSlot.InnerTextAsync()}");
 
                     }
 
